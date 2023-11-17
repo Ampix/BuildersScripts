c549a5e5243685abb16af47789d2541eed571e31
HVCsano <hvcsano@ampix.hu>
2023-11-17 17:55:40 +0100

    fix

--- updater.lua
+++ updater.lua
@@ -3,8 +3,8 @@ local baseUrl = "https://raw.githubusercontent.com/Ampix/BuildersScripts/master/
 if(fs.exists("versions.lua") == false) then
     write("Verziókezelő nem elérhető")
     local verlua = io.open("versions.lua","w+")
-    verlua.write(http.get(baseUrl .. "versions.lua").readAll())
-    verlua.close()
+    verlua:write(http.get(baseUrl .. "versions.lua").readAll())
+    verlua:close()
 end
 
 local ver = require("updater")
