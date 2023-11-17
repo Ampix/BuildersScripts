e58f46a31a4368789360161ea285642ea952c18f
HVCsano <hvcsano@ampix.hu>
2023-11-17 18:52:05 +0100

    jani

--- updater.lua
+++ updater.lua
@@ -1,26 +1,16 @@
 local baseUrl = "https://raw.githubusercontent.com/Ampix/BuildersScripts/master/"
 
 term.clear()
-
-if not fs.exists("versions.lua") then
-    term.setCursorPos(1,1)
-    write("Verziókezelö nem elérhetö")
-    sleep(1)
-    term.setCursorPos(1,2)
-    write("Letöltés...")
-    local verlua = io.open("versions.lua","w+")
-    local file = http.get(baseUrl .. "versions.lua")
-    if file then
-        local content = file.readAll()
-        file.close()
-        verlua:write(content)
-        verlua:close()
-        term.setCursorPos(1,3)
-        write("Letöltés sikeres")
-    end
+local verlua = io.open("ampix/versions.lua","w+")
+local file = http.get(baseUrl .. "versions.lua")
+if file then
+    local content = file.readAll()
+    file.close()
+    verlua:write(content)
+    verlua:close()
 end
 
-local ver = require("versions")
+local ver = require("ampix/versions")
 if ver then
     term.clear()
     term.setCursorPos(1,1)
