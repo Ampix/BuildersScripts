29a0d83fa9cce765f28e1e8aaaca03d4a0e74ba5
HVCsano <hvcsano@ampix.hu>
2023-11-17 17:32:48 +0100

    alakul

--- updater.lua
+++ updater.lua
@@ -1,3 +1,10 @@
+local baseUrl = "https://raw.githubusercontent.com/Ampix/BuildersScripts/master/"
+
+if(fs.exists("versions.lua") == false) then
+    write("Verziókezelő nem elérhető")
+    commands.exec("wget '"..baseUrl.."versions.lua' versions.lua")
+end
+
 local ver = require("updater")
 
 write("Ampix Builders Scripts v" .. ver.updater)
